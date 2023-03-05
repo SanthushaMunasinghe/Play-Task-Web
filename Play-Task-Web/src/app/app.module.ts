661a138b87a3df981cce050e2bbec2a21ee96e32
Santhusha Munasinghe <santhushamunasinghe601@gmail.com>
2023-03-05 22:21:57 +0530

    Institution-registration-page

--- Play-Task-Web/src/app/app.module.ts
+++ Play-Task-Web/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NavbarDefaultComponent } from './components/navbar-default/navbar-default.component';
 import { AboutPageComponent } from './about-page/about-page.component';
@@ -11,8 +15,22 @@ import { FooterDefaultComponent } from './components/footer-default/footer-defau
 import { RegisterPageComponent } from './register-page/register-page.component';
 
 @NgModule({
-  declarations: [AppComponent, LandingPageComponent, NavbarDefaultComponent, AboutPageComponent, LoginPageComponent, FooterDefaultComponent, RegisterPageComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    LandingPageComponent,
+    NavbarDefaultComponent,
+    AboutPageComponent,
+    LoginPageComponent,
+    FooterDefaultComponent,
+    RegisterPageComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- Play-Task-Web/src/app/register-page/register-page.component.ts
+++ Play-Task-Web/src/app/register-page/register-page.component.ts
@@ -1,10 +1,85 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+interface InstitutionResponse {
+  institutionId: string;
+}
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styleUrls: ['./register-page.component.css']
+  styleUrls: ['./register-page.component.css'],
 })
 export class RegisterPageComponent {
+  registrationForm = this.formBuilder.group({
+    name: '',
+    type: '',
+    email: '',
+    contactno: '',
+    location: '',
+    password: '',
+    confirmedPassword: '',
+  });
 
+  types: string[] = ['School', 'Primary School', 'Tution'];
+
+  registrationErrors: string[] = [''];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private http: HttpClient
+  ) {}
+
+  onSubmit(): void {
+    const formInstitution = this.registrationForm.value;
+    this.registrationErrors = [];
+
+    for (let value of Object.values(formInstitution)) {
+      if (value == '') {
+        this.registrationErrors.push('Fill All Fields!');
+        break;
+      }
+    }
+
+    const passwordPattern =
+      /^(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])(?=.*[a-zA-Z]).{8,}$/;
+
+    if (formInstitution.password) {
+      if (formInstitution.password.length < 8) {
+        this.registrationErrors.push('Password Must Have 8-16 Characters');
+      } else if (!passwordPattern.test(formInstitution.password)) {
+        this.registrationErrors.push('Weak Password!');
+      } else if (
+        formInstitution.password != formInstitution.confirmedPassword
+      ) {
+        this.registrationErrors.push('Password Does Not Match!');
+      }
+    }
+
+    if (this.registrationErrors.length == 0) {
+      const inst = {
+        name: formInstitution.name,
+        type: formInstitution.type,
+        location: formInstitution.location,
+        email: formInstitution.email,
+        contactno: formInstitution.contactno,
+        password: formInstitution.password,
+      };
+
+      this.http.post<InstitutionResponse>('/api/institutions', inst).subscribe(
+        (res) => {
+          console.log(res);
+          this.registrationForm.reset();
+          // this.router.navigate(['/dashboard', res.userId]);
+          console.log(res.institutionId);
+        },
+        (error) => {
+          this.registrationErrors.push(error.error.message);
+        }
+      );
+    }
+  }
 }
